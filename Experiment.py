6184f50aa1b9843954e4f2e74d2274483a816cb1
jorge <jorgenunezrivera@gmail.com>
2023-07-06 18:22:01 +0200

    experiment descomentado

--- Experiment.py
+++ Experiment.py
@@ -2,7 +2,7 @@ import os
 
 from Cleverhans_densenet import densenet_generate, densenet_adversarial_test
 from Cleverhans_recommender import recomender_generate, recomender_adversarial_test
-from Recommender import recommender_data_preparation
+from Recommender import recommender_data_preparation, recommender_train, recommender_test
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
@@ -17,7 +17,7 @@ tf.autograph.set_verbosity(0)
 
 ############################# DATA PREPARATION ###################################################
 
-#recommender_data_preparation(original_database_path,generate_deep_features=True,download_images=True)
+recommender_data_preparation(original_database_path,generate_deep_features=True,download_images=True,scale_images=True)
 '''
 #Genera los dfs (data/dataframes/test_images_df,train_images_df,worst_images_df)
 #Descarga las imagenes (images_shared)
@@ -33,29 +33,23 @@ Guarda las gráficas de entrenamiento (figures/recommender_augmentation_[False /
 '''
 
 
-#recommender_train(training_epochs=40, lr=0.00001,execute_augmentation=False, model_type=1)
-#recommender_train(training_epochs=40,lr=0.00001,execute_augmentation=False,model_type=2)
-#recommender_train(training_epochs=40,lr=0.00001,execute_augmentation=False,model_type=3)
+recommender_train(training_epochs=40, lr=0.00001,execute_augmentation=False, model_type=1)
+recommender_train(training_epochs=40,lr=0.00001,execute_augmentation=False,model_type=2)
+recommender_train(training_epochs=40,lr=0.00001,execute_augmentation=False,model_type=3)
 
-#recommender_train(training_epochs=40, lr=0.00001, execute_augmentation=True, model_type=1)
-#recommender_train(training_epochs=40,lr=0.00001,execute_augmentation=True,model_type=2)
-#recommender_train(training_epochs=40,lr=0.00001,execute_augmentation=True,model_type=3)
+recommender_train(training_epochs=40, lr=0.00001, execute_augmentation=True, model_type=1)
+recommender_train(training_epochs=40,lr=0.00001,execute_augmentation=True,model_type=2)
+recommender_train(training_epochs=40,lr=0.00001,execute_augmentation=True,model_type=3)
 
 ########################## RECOMMENDER TEST ############################################################
 '''
 Prueba los modelos  del recomendador
 
 '''
-#recommender_test(augmentation=True,test_set=Test_set.BALANCED,model_type=1)
-#recommender_test(augmentation=True,test_set=Test_set.BALANCED,model_type=2)
-#recommender_test(augmentation=True,test_set=Test_set.BALANCED,model_type=3)
+recommender_test(augmentation=True,test_set=Test_set.BALANCED,model_type=1)
+recommender_test(augmentation=True,test_set=Test_set.BALANCED,model_type=2)
+recommender_test(augmentation=True,test_set=Test_set.BALANCED,model_type=3)
 
-#recommender_test(augmentation=True,test_set=Test_set.FULL,model_type=1)
-#recommender_test(augmentation=True,test_set=Test_set.WORST,model_type=1)
-#recommender_test(augmentation=True,test_set=Test_set.BALANCED,model_type=2)
-#recommender_test(augmentation=False,test_set=Test_set.BALANCED,model_type=1)
-#recommender_test(augmentation=True,test_set=Test_set.BALANCED,model_type=2)
-#recommender_test(augmentation=True,test_set=Test_set.BALANCED,model_type=3)
 
 
 '''
@@ -65,16 +59,16 @@ Guarda los pesos en data/weights/annomnaly_detector_weights[1-3]
 Guarda las gráficas de entrenamiento en figures/annomaly_detector_train
 '''
 
-#anomaly_detector_train(cl_recommender_test_features_path[1],epochs=500,lr=0.0001,model_n=1)
-#anomaly_detector_train(cl_recommender_test_features_path[1],epochs=500,lr=0.0001,model_n=2)
-#anomaly_detector_train(cl_recommender_test_features_path[1],epochs=500,lr=0.0001,model_n=3)
+anomaly_detector_train(cl_recommender_test_features_path[1],epochs=500,lr=0.0001,model_n=1)
+anomaly_detector_train(cl_recommender_test_features_path[1],epochs=500,lr=0.0001,model_n=2)
+anomaly_detector_train(cl_recommender_test_features_path[1],epochs=500,lr=0.0001,model_n=3)
 
 
 ############################################ GENERATING ADVERSARIOUS SAMPLES #######################################
-#for eps in (0.005,0.007,0.009):
-#    print(f"eps: {eps}")
-#    recomender_generate(augmentation=True,test_set=Test_set.FULL,fgm=False,pgd=True,eps=3.0,eps_iter=eps,n_iter=40)
-#    recomender_adversarial_test(augmentation=True,test_set=Test_set.FULL,eps_iter=eps)
+for eps in (0.005,0.007,0.009):
+    print(f"eps: {eps}")
+    recomender_generate(augmentation=True,test_set=Test_set.FULL,fgm=False,pgd=True,eps=3.0,eps_iter=eps,n_iter=40)
+    recomender_adversarial_test(augmentation=True,test_set=Test_set.FULL,eps_iter=eps)
 
 '''
 Genera ejemplos de features adversarias del conjunto de test (o del conjunto worst de las reviews negativas)
@@ -85,17 +79,16 @@ guarda las features en features\cl_recommender_test_features_model[1-3].npy
 #DENSENET GENERATE WORST
 print("densenet worst")
 for eps in (0.03,0.05,0.1):
-#eps = 0.03
     print(f"eps iter: {eps}")
     densenet_generate(augmentation=True,worst=True,fgm=False,pgd=True,eps=30.0,eps_iter=eps,n_iter=30 ,num_images=256) #EL QUE MANDA ES EPS ITER
     densenet_adversarial_test(augmentation=True,worst=True,num_images=256,eps_iter=eps)
 
 #DENSENET GENERATE TEST
-#print("densenet test")
-#for eps in (0.03,0.05,0.1):
-#    print(f"eps iter: {eps}")
-#    densenet_generate(augmentation=True,worst=False,fgm=False,pgd=True,eps=30.0,eps_iter=eps,n_iter=30 ,num_images=256) #EL QUE MANDA ES EPS ITER
-#    densenet_adversarial_test(augmentation=True,worst=False,num_images=256,eps_iter=eps)
+print("densenet test")
+for eps in (0.03,0.05,0.1):
+    print(f"eps iter: {eps}")
+    densenet_generate(augmentation=True,worst=False,fgm=False,pgd=True,eps=30.0,eps_iter=eps,n_iter=30 ,num_images=256) #EL QUE MANDA ES EPS ITER
+    densenet_adversarial_test(augmentation=True,worst=False,num_images=256,eps_iter=eps)
 
 
 
@@ -109,20 +102,20 @@ y los features en features\cl_densenet_[test/worst]_features_model[1-3].npy
 '''
 ######################## TEST ANNOMALY DETECTOT RECOMMENDER ###############################
 print("recommender test")
-#for eps in (0.005,0.007,0.009):
-#    print(f"eps = {eps}")
-#    for mdl in (1, 2, 3):
-#        anomaly_detector_test(cl_recommender_test_features_path[1], model_n=mdl, worst=False,clipplot=1,eps_iter=eps)
+for eps in (0.005,0.007,0.009):
+    print(f"eps = {eps}")
+    for mdl in (1, 2, 3):
+        anomaly_detector_test(cl_recommender_test_features_path[1], model_n=mdl, worst=False,clipplot=1,eps_iter=eps)
 
 ############################################ TEST ANOMALY DETECTOR DENSENET#####################################################
-#print("densenet test")
-#for eps in (0.03,0.05,0.1):
-#    for mdl in (1,2,3):
-#        anomaly_detector_test(cl_densenet_test_features_path[1],model_n=mdl,worst=False,eps_iter=eps,clipplot=1)
+print("densenet test")
+for eps in (0.03,0.05,0.1):
+    for mdl in (1,2,3):
+        anomaly_detector_test(cl_densenet_test_features_path[1],model_n=mdl,worst=False,eps_iter=eps,clipplot=1)
 
 
 ###############WORST FEATURES CON EPS=0.03,0.05,0.1
-#print("densenet worst")
-#for eps in (0.03,0.05,0.1):
-#    for mdl in (1,2,3):
-#        anomaly_detector_test(cl_densenet_worst_features_path ,model_n=mdl,worst=True,eps_iter=eps)
+print("densenet worst")
+for eps in (0.03,0.05,0.1):
+    for mdl in (1,2,3):
+        anomaly_detector_test(cl_densenet_worst_features_path ,model_n=mdl,worst=True,eps_iter=eps)
